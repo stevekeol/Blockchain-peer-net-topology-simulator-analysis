0103de5704a3ef052b2fd89bcbdeee0cb9b011a6
stevekeol <stevekeol.x@gmail.com>
2021-04-13 19:39:06 +0800

    :art:

---
+++ src/Datas/CanvasTest.ts
@@ -0,0 +1,377 @@
+  const data = {
+    nodes: [
+      {
+        id: '0',
+        label: 'node-0',
+        type: 'scale-animation',
+        size: 80,        
+        cluster: 'a',
+      },
+      {
+        id: '1',
+        label: 'node-1',
+        cluster: 'a',
+      },
+      {
+        id: '2',
+        label: 'node-2',
+        cluster: 'a',
+      },
+      {
+        id: '3',
+        label: 'node-3',
+        cluster: 'a',
+      },
+      {
+        id: '4',
+        label: 'node-4',
+        cluster: 'a',
+      },
+      {
+        id: '5',
+        label: 'node-5',
+        cluster: 'a',
+      },
+      {
+        id: '6',
+        label: 'node-6',
+        cluster: 'a',
+      },
+      {
+        id: '7',
+        label: 'node-7',
+        cluster: 'a',
+      },
+      {
+        id: '8',
+        label: 'node-8',
+        cluster: 'a',
+      },
+      {
+        id: '9',
+        label: 'node-9',
+        cluster: 'a',
+      },
+      {
+        id: '10',
+        label: 'node-10',
+        cluster: 'a',
+      },
+      {
+        id: '11',
+        label: 'node-11',
+        cluster: 'a',
+      },
+      {
+        id: '12',
+        label: 'node-12',
+        cluster: 'a',
+      },
+      {
+        id: '13',
+        label: 'node-13',
+        cluster: 'b',
+      },
+      {
+        id: '14',
+        label: 'node-14',
+        cluster: 'b',
+      },
+      {
+        id: '15',
+        label: 'node-15',
+        cluster: 'b',
+      },
+      {
+        id: '16',
+        label: 'node-16',
+        cluster: 'b',
+      },
+      {
+        id: '17',
+        label: 'node-17',
+        cluster: 'b',
+      },
+      {
+        id: '18',
+        label: 'node-18',
+        cluster: 'c',
+      },
+      {
+        id: '19',
+        label: 'node-19',
+        cluster: 'c',
+      },
+      {
+        id: '20',
+        label: 'node-20',
+        cluster: 'c',
+      },
+      {
+        id: '21',
+        label: 'node-21',
+        cluster: 'c',
+      },
+      {
+        id: '22',
+        label: 'node-22',
+        cluster: 'c',
+      },
+      {
+        id: '23',
+        label: 'node-23',
+        cluster: 'c',
+      },
+      {
+        id: '24',
+        label: 'node-24',
+        cluster: 'c',
+      },
+      {
+        id: '25',
+        label: 'node-25',
+        cluster: 'c',
+      },
+      {
+        id: '26',
+        label: 'node-26',
+        cluster: 'c',
+      },
+      {
+        id: '27',
+        label: 'node-27',
+        cluster: 'c',
+      },
+      {
+        id: '28',
+        label: 'node-28',
+        cluster: 'c',
+      },
+      {
+        id: '29',
+        label: 'node-29',
+        cluster: 'c',
+      },
+      {
+        id: '30',
+        label: 'node-30',
+        cluster: 'c',
+      },
+    ],
+    edges: [
+      {
+        source: '0',
+        target: '1',
+      },
+      {
+        source: '0',
+        target: '2',
+      },
+      {
+        source: '0',
+        target: '3',
+      },
+      {
+        source: '0',
+        target: '4',
+      },
+      {
+        source: '0',
+        target: '5',
+      },
+      {
+        source: '0',
+        target: '7',
+      },
+      {
+        source: '0',
+        target: '8',
+      },
+      {
+        source: '0',
+        target: '9',
+      },
+      {
+        source: '0',
+        target: '10',
+      },
+      {
+        source: '0',
+        target: '11',
+      },
+      {
+        source: '0',
+        target: '13',
+      },
+      {
+        source: '0',
+        target: '14',
+      },
+      {
+        source: '0',
+        target: '15',
+      },
+      {
+        source: '0',
+        target: '16',
+      },
+      {
+        source: '2',
+        target: '3',
+      },
+      {
+        source: '4',
+        target: '5',
+      },
+      {
+        source: '4',
+        target: '6',
+      },
+      {
+        source: '5',
+        target: '6',
+      },
+      {
+        source: '7',
+        target: '13',
+      },
+      {
+        source: '8',
+        target: '14',
+      },
+      {
+        source: '9',
+        target: '10',
+      },
+      {
+        source: '10',
+        target: '22',
+      },
+      {
+        source: '10',
+        target: '14',
+      },
+      {
+        source: '10',
+        target: '12',
+      },
+      {
+        source: '10',
+        target: '24',
+      },
+      {
+        source: '10',
+        target: '21',
+      },
+      {
+        source: '10',
+        target: '20',
+      },
+      {
+        source: '11',
+        target: '24',
+      },
+      {
+        source: '11',
+        target: '22',
+      },
+      {
+        source: '11',
+        target: '14',
+      },
+      {
+        source: '12',
+        target: '13',
+      },
+      {
+        source: '16',
+        target: '17',
+      },
+      {
+        source: '16',
+        target: '18',
+      },
+      {
+        source: '16',
+        target: '21',
+      },
+      {
+        source: '16',
+        target: '22',
+      },
+      {
+        source: '17',
+        target: '18',
+      },
+      {
+        source: '17',
+        target: '20',
+      },
+      {
+        source: '18',
+        target: '19',
+      },
+      {
+        source: '19',
+        target: '20',
+      },
+      {
+        source: '19',
+        target: '22',
+      },
+      {
+        source: '19',
+        target: '23',
+      },
+      {
+        source: '20',
+        target: '21',
+      },
+      {
+        source: '21',
+        target: '22',
+      },
+      {
+        source: '22',
+        target: '24',
+      },
+      {
+        source: '22',
+        target: '25',
+      },
+      {
+        source: '22',
+        target: '26',
+      },
+      {
+        source: '22',
+        target: '23',
+      },
+      {
+        source: '22',
+        target: '28',
+      },
+      {
+        source: '22',
+        target: '30',
+      },
+      {
+        source: '23',
+        target: '28',
+      },
+      {
+        source: '23',
+        target: '27',
+      },
+      {
+        source: '23',
+        target: '29',
+      },
+      {
+        source: '23',
+        target: '30',
+      },
+    ],
+  };
+
+  export default data;
